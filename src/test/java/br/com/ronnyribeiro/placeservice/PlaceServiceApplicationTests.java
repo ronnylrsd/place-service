16360540f72ae1d13cbb6cdb227028296fad0079
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-07-19 09:02:32 -0300

    Feat: create place-service project

---
+++ src/test/java/br/com/ronnyribeiro/placeservice/PlaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ronnyribeiro.placeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
