8c6acc5b529ca8904fc4242de005779fd7df4f40
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-07-20 10:57:05 -0300

    Feat: add service

---
+++ src/main/java/br/com/ronnyribeiro/placeservice/domain/PlaceService.java
@@ -0,0 +1,15 @@
+package br.com.ronnyribeiro.placeservice.domain;
+
+import reactor.core.publisher.Mono;
+
+public class PlaceService {
+    private PlaceRepository placeRepository;
+
+    public PlaceService(PlaceRepository placeRepository) {
+        this.placeRepository = placeRepository;
+    }
+    
+    public Mono<Place> create(Place place){
+        return placeRepository.save(place);
+    }
+}
