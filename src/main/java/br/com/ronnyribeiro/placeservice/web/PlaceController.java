124d7c71a43607c2f94c1e9b4a2c149c4ffb3251
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-07-24 13:39:39 -0300

    Feat: create placerequest

--- src/main/java/br/com/ronnyribeiro/placeservice/web/PlaceController.java
+++ src/main/java/br/com/ronnyribeiro/placeservice/web/PlaceController.java
@@ -3,6 +3,7 @@ package br.com.ronnyribeiro.placeservice.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class PlaceController {
   }
 
   @PostMapping
-  public ResponseEntity<Mono<Place>> create(Place place){
+  public ResponseEntity<Mono<Place>> create(@RequestBody Place place){
     var createdPlace = placeservice.create(place);
     return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace);
   }
