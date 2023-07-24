939edc5f12fec3c423e25c618ff56fa89b6ef58d
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-07-24 14:39:57 -0300

    Refactor: adding slugify

--- src/main/java/br/com/ronnyribeiro/placeservice/domain/PlaceService.java
+++ src/main/java/br/com/ronnyribeiro/placeservice/domain/PlaceService.java
@@ -1,17 +1,21 @@
 package br.com.ronnyribeiro.placeservice.domain;
 
+import com.github.slugify.Slugify;
+
 import br.com.ronnyribeiro.placeservice.api.PlaceRequest;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
+    private Slugify slg;
 
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
     
     public Mono<Place> create(PlaceRequest placeRequest){
-        var place = new Place(null, placeRequest.name(), placeRequest.slug(), placeRequest.state(), placeRequest.createdAt(), placeRequest.updatedAt());
+        var place = new Place(null, placeRequest.name(), slg.slugify(placeRequest.name()), placeRequest.state(), placeRequest.createdAt(), placeRequest.updatedAt());
         return placeRepository.save(place);
     }
 }
